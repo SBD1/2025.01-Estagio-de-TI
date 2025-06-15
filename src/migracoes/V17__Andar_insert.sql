947d3873465c7788a0f4408fbcf7e5e054941b5a
lucasarruda9 <lucasmendoncaarruda5@gmail.com>
2025-06-15 15:36:11 -0300

    Aplicando insercoes corretas nas tabelas andares e salas e tirando criação de tabelas duplicadas

--- src/migracoes/V17__Andar_insert.sql
+++ src/migracoes/V17__Andar_insert.sql
@@ -1,14 +1,14 @@
-INSERT INTO Andar(id_andar, id_mundo, numero, nome, tema, descricao) VALUES
-(1, 1, 1, 'Suporte básico', 'Help Desk, reino das perguntas óbvias.', 'nada'),
-(2, 1, 2, 'Problemas de Hardware', '1', 'nada'),
-(3, 1, 3, 'c', '2', 'nada'),
-(4, 1, 4, 'd', '3', 'nada'),
-(5, 1, 5, 'e', '4', 'nada'),
-(6, 1, 6, 'f', '5', 'nada'),
-(7, 1, 7, 'g', '6', 'nada'),
-(8, 1, 8, 'h', '7', 'nada'),
-(9, 1, 9, 'i', '8', 'nada'),
-(10, 1, 10, 'j', '9', 'nada'),
-(11, 1, -2, 'k', '10', 'nada'),
-(12, 1, -1, 'l', '11', 'nada'),
-(13, 1, 0, 'm', '12', 'nada');
+INSERT INTO Andar(id_andar, id_mundo, numero, nome) VALUES
+(11, 1, -2, 'Subsolo 2: Data Center'),
+(12, 1, -1, 'Subsolo 1: Almoxarifado'),
+(13, 1, 0, 'Térreo: Recepção'),
+(1, 1, 1, 'Andar 1: Suporte Básico'),
+(2, 1, 2, 'Andar 2: Problemas de Hardware'),
+(3, 1, 3, 'Andar 3: Suporte Remoto'),
+(4, 1, 4, 'Andar 4: Infraestrutura'),
+(5, 1, 5, 'Andar 5: Redes'),
+(6, 1, 6, 'Andar 6: Segurança'),
+(7, 1, 7, 'Andar 7: Desenvolvimento Web'),
+(8, 1, 8, 'Andar 8: Backend'),
+(9, 1, 9, 'Andar 9: DevOps'),
+(10, 1, 10, 'Andar 10: Diretoria');
