5ad299bbb46af8c24a32f7510b5bfe0c010dafbd
lucasarruda9 <lucasmendoncaarruda5@gmail.com>
2025-06-15 15:25:00 -0300

    ajustando tabelas estagiario sala conexoes e andar

--- src/migracoes/V2__Andar_table.sql
+++ src/migracoes/V2__Andar_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS Andar (
     id_mundo INT NOT NULL, 
     FOREIGN KEY (id_mundo) REFERENCES Mundo(id_mundo),
     numero SMALLINT NOT NULL, -- -2 = Subsolo 2, -1 = Subsolo 1, 0 = Térreo, 1~10 = Andares
-    nome VARCHAR(100) NOT NULL,
-    tema VARCHAR(200) NOT NULL,
-    descricao TEXT NOT NULL
+    nome VARCHAR(100) NOT NULL
+    
 );
+
